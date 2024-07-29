f951d51eb42de2183150eb2e30bc3ec3b8b9cef3
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2024-07-30 09:26:59 +1000

    created types files. Classes and Interfaces should be on a separate folder

---
+++ types/location.type.ts
@@ -0,0 +1,22 @@
+export type LocationType = {
+  streetAddress: string;
+  city: string;
+  state: string;
+  appliances: LocationApplianceType[];
+}
+
+/**
+ * An appliance within a location. Also tracks the number of appliances, the
+ * hours its running, and the total watt hour per day.
+ * 
+ * @property {string} name Name of appliance
+ * @property {number} quantity Number of the same appliance in a location
+ * @property {number} hoursPerDay Number of hours the appliances are running per day.
+ * @property {number} totalWHSPerDay Calculated watt-hours per day for this appliance object. (watt * hoursPerDay) * quantity where watt is a constant fetch from server
+ */
+type LocationApplianceType = {
+  name: string;
+  quantity: number;
+  hoursPerDay: number;
+  totalWHSPerDay: number;
+}
