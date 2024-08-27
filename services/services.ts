8b10f1628d2b843cba3f1ab13626f53361111956
MisterMunchkin <dalmiet@gmail.com>
2024-08-27 14:19:57 +1000

    created getAppliances request

--- services/services.ts
+++ services/services.ts
@@ -1,4 +1,5 @@
 import { API_CONTROLLER } from "@/constants/controller-navigation.constants";
+import { ApplianceType } from "@/types/appliance.type";
 
 namespace Services {
   export const getStates = async () => {
@@ -12,6 +13,18 @@ namespace Services {
 
     const states = await res.json() as string[];
     return states;
+  }
+  export const getAppliances = async () => {
+    const reqUrl = `${API_CONTROLLER}appliances`;
+    const res = await fetch(reqUrl, {
+      method: 'GET',
+    });
+
+    if (!res.ok)
+      console.error('appliances API threw an error', res.status);
+
+    const appliances = await res.json() as ApplianceType[];
+    return appliances;
   }
 }
 
