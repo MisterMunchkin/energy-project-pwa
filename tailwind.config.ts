ef4a766d4a1bf3f7d670d91f4cff44456e7b1146
MisterMunchkin <dalmiet@gmail.com>
2024-09-03 20:55:13 +1000

    tailwind config automatically updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/modal.js"
+    "./node_modules/@nextui-org/theme/dist/components/(dropdown|modal|menu|divider|popover|button|ripple|spinner).js"
   ],
   theme: {
     extend: {
