c83b93b1a9cefc5d4ee53fc8a0fea22fd1e5234c
MisterMunchkin <dalmiet@gmail.com>
2024-09-03 11:56:59 +1000

    created a field wrapper and built a clsx helper type to control classnames of a wrapper component

---
+++ components/wrappers/FieldWrapper.tsx
@@ -0,0 +1,50 @@
+/**
+ * Handles displaying validation messages from Formik
+ */
+
+import { ClassValues } from "@/lib/clsx";
+import { cn } from "@nextui-org/theme";
+import { Field } from "formik";
+import { ReactNode } from "react"
+
+type Props = {
+  name: string;
+  label: string;
+  component: string;
+  children?: ReactNode;
+  validation?: () => string | undefined;
+  errors?: string;
+  touched?: boolean;
+  classNames?: ClassValues<"container" | "label" | "field" | "error">
+}
+const FieldWrapper = ({name, label, component, children, validation, errors, touched, classNames}: Props) => {
+  return <div className={cn(
+      "default-field-container",
+      classNames?.container
+    )}>
+    <label 
+      htmlFor={name}
+      className={cn(
+        "",
+        classNames?.label
+      )}
+    >
+      {label}
+    </label>
+    <Field
+      className={cn(
+        "",
+        classNames?.field
+      )}
+      id={name}
+      name={name}
+      component={component}
+      validate={validation}
+    >
+      {children}
+    </Field>
+    {errors && touched && <div className={cn("", classNames?.error)}>{errors}</div>}
+  </div>
+}
+
+export default FieldWrapper;
