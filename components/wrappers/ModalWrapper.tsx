d1c8768e8642ddb4d1dcd981e341f9526cab02d0
MisterMunchkin <dalmiet@gmail.com>
2024-09-03 12:16:50 +1000

    added comments to modal wrapper

--- components/wrappers/ModalWrapper.tsx
+++ components/wrappers/ModalWrapper.tsx
@@ -1,9 +1,15 @@
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/modal";
 import { FC, ReactNode } from "react";
 
+/**
+ * The type ModalTriggerComponents will have to follow to use this wrapper
+ */
 export type ModalTriggerComponentType = {
   onOpen: () => void;
 }
+/**
+ * The type FooterComponent will have to follow to use this wrapper.
+ */
 export type ModalFooterComponentType = {
   onClose: () => void;
   onSubmit?: () => void;
@@ -21,6 +27,12 @@ type Props = {
  * 
  * Requires a FooterComponent to handle onClose and a ModalTriggerComponent
  * to handle onOpen.
+ * 
+ * @param {title} title The header of the modal
+ * @param {children} children The ReactNode rendered within the ModalBody
+ * @param {FooterComponent} FooterComponent The Functional Component that will be rendered within the ModalFooter
+ * @param {ModalTriggerComponent} ModalTriggerComponent The Functional Component that will trigger the opening of the modal
+ * @param {onSubmit} onSubmit An optional function that gets called if onSubmit is actioned within the FooterComponent
  */
 const ModalWrapper = ({title, children, FooterComponent, ModalTriggerComponent, onSubmit}: Props) => {
   const {
