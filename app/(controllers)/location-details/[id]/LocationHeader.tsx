503b3e60a8431a56efbdf457989368dbf41b631c
MisterMunchkin <dalmiet@gmail.com>
2024-09-03 20:55:50 +1000

    moved to LocationHeader

---
+++ app/(controllers)/location-details/[id]/LocationHeader.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import DropdownWrapper, { SimpleDropdownItemType } from "@/components/wrappers/DropdownWrapper"
+import Link from "next/link"
+import { Key } from "react"
+import { BsThreeDots } from "react-icons/bs"
+import { TbWorldUpload } from "react-icons/tb"
+import { VscArrowLeft, VscEdit, VscTrash } from "react-icons/vsc"
+
+const locationMenuItems: SimpleDropdownItemType[] = [
+  {
+    display: "Post",
+    props: {
+      key: 'post',
+      description: 'Uploads this location to the public leaderboard',
+      startContent: <TbWorldUpload className="w-6 h-6" />
+    }
+  },
+  {
+    display: "Edit",
+    props: {
+      key: 'edit',
+      startContent: <VscEdit className="w-6 h-6" />
+    }
+  },
+  {
+    display: "Delete",
+    props: {
+      key: 'delete',
+      startContent: <VscTrash className="text-red-600 w-6 h-6" />,
+      className: "text-red-600"
+    }
+  }
+];
+
+type Props = {
+  locationId: string
+}
+const LocationHeader = ({locationId}: Props) => {
+  function handleDropDownMenuAction(key: Key): void {
+    console.log(key)
+  }
+
+  return (
+    <div className="p-2 flex flex-row justify-between items-center">
+      <Link
+        className="cursor-pointer"
+        href='/'
+      >
+        <VscArrowLeft
+          className="text-epp-indigo w-10 h-10"
+        />
+      </Link>
+      <DropdownWrapper 
+        trigger={(
+          <button
+            type="button"
+          >
+            <BsThreeDots className="w-10 h-10 text-epp-indigo" />
+          </button>
+        )}
+        ariaLabel="Location Menu"
+        simpleMenuItems={locationMenuItems}
+        onAction={handleDropDownMenuAction}
+      />
+    </div>
+  )
+}
+
+export default LocationHeader;
--- app/(controllers)/location-details/[id]/page.tsx
+++ app/(controllers)/location-details/[id]/page.tsx
@@ -1,17 +1,20 @@
 import Appliances from "@/components/appliance/Appliances";
 import LocationBarChart from "@/components/location/LocationBarChart";
 import LocationStats from "@/components/location/LocationStats";
+import DropdownWrapper, { SimpleDropdownItemType } from "@/components/wrappers/DropdownWrapper";
 import { ServerComponentProps } from "@/types/server-component-props.types";
 import Link from "next/link";
+import { Key } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { TbWorldUpload } from "react-icons/tb";
 import { VscArrowLeft } from "react-icons/vsc";
-
+import LocationHeader from "./LocationHeader";
 
 /**
  * @param {serverComponentProps} serverComponentProps Strictly types NextJs server navigation from a `<Link />`. 
  * 
 */
 const LocationDetails = async ({params}: ServerComponentProps) => {
-
   const {
     id: idString
   } = params || {};
@@ -23,16 +26,9 @@ const LocationDetails = async ({params}: ServerComponentProps) => {
   >
     {/* Chart, back button, and menu */}
     <div className="flex flex-col ">
-      <div className="p-2">
-        <Link
-          className="cursor-pointer w-10"
-          href='/'
-        >
-          <VscArrowLeft
-            className="text-epp-indigo w-10 h-10"
-          />
-        </Link>
-      </div>
+      <LocationHeader 
+        locationId={idString}
+      />
       <LocationBarChart locationId={idString} />
     </div>
     {/* Location Stats and Appliances List */}
