12c8a44a8e68dc1a54a2af7bd4959ddcf2b5b55b
MisterMunchkin <dalmiet@gmail.com>
2024-09-03 20:55:27 +1000

    created dropdown wrapper

---
+++ components/wrappers/DropdownWrapper.tsx
@@ -0,0 +1,39 @@
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
+import { Key, ReactNode } from "react";
+
+export type SimpleDropdownItemType = {
+  display: string;
+  props: {
+    key: string;
+    description?: string
+    startContent?: ReactNode;
+    className?: string;
+  }
+}
+type Props = {
+  trigger: ReactNode
+  ariaLabel: string;
+  simpleMenuItems: SimpleDropdownItemType[]; 
+  onAction: (key: Key) => void;
+}
+const DropdownWrapper = ({trigger, ariaLabel, simpleMenuItems, onAction}: Props) => {
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        {trigger}
+      </DropdownTrigger>
+      <DropdownMenu 
+        aria-label={ariaLabel}
+        onAction={onAction}
+      >
+        {simpleMenuItems.map(({display, props}) => (
+          <DropdownItem
+            {...props}
+          >{display}</DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+export default DropdownWrapper;
