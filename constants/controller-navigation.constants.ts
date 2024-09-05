c1e0017b1dfe9142eebfd381a660086ee7ee8e6e
MisterMunchkin <dalmiet@gmail.com>
2024-09-05 20:28:24 +1000

    added API public leaderboard constant

--- constants/controller-navigation.constants.ts
+++ constants/controller-navigation.constants.ts
@@ -1,2 +1,3 @@
 export const API_CONTROLLER = process.env.NEXT_PUBLIC_BASE_URL + '/api/controllers/';
+export const API_PUBLIC_LEADERBOARD = API_CONTROLLER + 'public-leaderboard';
 export const LOCATION_DETAILS = '/location-details';
