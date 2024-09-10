d0f1c1a07cdd6ab4ecaa7df8edd7ea979de75ee9
MisterMunchkin <dalmiet@gmail.com>
2024-09-10 12:59:52 +1000

    created formatter for address

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -1,3 +1,5 @@
+import { LocationType } from "@/types/location.type";
+
 /**
  * Gets the ordinal of a given number.
  * @example getOrdinal(123) returns "123rd"
@@ -8,4 +10,8 @@ export const getOrdinal = (n: number) => {
   const s = ["th", "st", "nd", "rd"];
   const v = n % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+export const formatAddress = ({streetAddress, city, state, postalCode}: Omit<LocationType, "appliances">) => {
+  return `${streetAddress}, ${city}, ${state} ${postalCode}`;
 }
