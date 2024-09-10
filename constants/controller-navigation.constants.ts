ba07c7bee80e326ddc8015969e64d30585f7f26a
MisterMunchkin <dalmiet@gmail.com>
2024-09-10 14:18:25 +1000

    added national statistics route to the constants

--- constants/controller-navigation.constants.ts
+++ constants/controller-navigation.constants.ts
@@ -2,6 +2,7 @@ export const API_CONTROLLER = process.env.NEXT_PUBLIC_BASE_URL + '/api/controlle
 export const API_PUBLIC_LEADERBOARD = API_CONTROLLER + 'public-leaderboard';
 export const LOCATION_DETAILS = '/location-details';
 export const PUBLIC_LEADERBOARD = '/public-leaderboard';
+export const NATIONAL_STATISTICS = `${PUBLIC_LEADERBOARD}/national-statistics`;
 
 export namespace RevalidatePath {
   export const LOCATION_DETAILS_PAGE = `(controllers)${LOCATION_DETAILS}/[id]`; 
