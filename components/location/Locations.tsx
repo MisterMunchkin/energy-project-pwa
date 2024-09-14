71d14a91ba4b3cc9f3da5566e4c7257eea78fd03
MisterMunchkin <dalmiet@gmail.com>
2024-09-14 22:59:31 +1000

    using Chip to see how it works

--- components/location/Locations.tsx
+++ components/location/Locations.tsx
@@ -6,11 +6,12 @@ import { LOCATION_DETAILS } from "@/constants/controller-navigation.constants";
 import { localService } from "@/services/local-service";
 import { useEffect, useState } from "react";
 import { LocationType } from "@/types/location.type";
-import { SelectableListProvider } from "@/components/test/selectable-list";
+import { SelectableListProvider, useSelectableList } from "@/components/test/selectable-list";
 import SelectableListItem from "@/components/test/selectable-list-item";
+import { Chip } from "@nextui-org/chip";
 
 type ListItem = {
-  value: string;
+  value: "QLD" | "NSW" | "VIC" | "TAS";
   label: string
 }
 
@@ -55,12 +56,12 @@ const Locations = () => {
     setLocations(locations);
   }, []);
 
-
   return <main className="flex min-h-screen flex-col items-center space-y-4 p-4">
     <SelectableListProvider
       // defaultValue={"QLD"}
     >
       {({selected}) => (
+
         <div className="flex flex-col">
           {sampleListItems.map(({value, label}, index) => (
             <SelectableListItem 
@@ -69,13 +70,14 @@ const Locations = () => {
               onSelectedClassName="bg-red-500 text-white"
               value={value}
             >
-              <span>{label}</span>
+              <Chip variant="dot">{label}</Chip>
             </SelectableListItem>
           ))}
 
           {JSON.stringify(selected)}
         </div>
       )}
+      
     </SelectableListProvider>
     
     {locations.map((location, index) => (
