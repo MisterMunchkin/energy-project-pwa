e1c104a656c38b9afac9af12522bdd96770dea3e
MisterMunchkin <dalmiet@gmail.com>
2024-09-29 14:34:24 +1000

    linter

--- app/(controllers)/public-leaderboard/national-statistics/page.tsx
+++ app/(controllers)/public-leaderboard/national-statistics/page.tsx
@@ -8,40 +8,30 @@ import Link from "next/link";
 import { Suspense } from "react";
 import { VscArrowLeft } from "react-icons/vsc";
 
-export default async function page () {
+export default async function page() {
   const stats = await PublicLeaderboardService.getNationalStatistics();
   const nationalStats = new NationalStatisticsClass(stats);
   const chartData = nationalStats.chartData;
   console.log(stats);
 
-  return <section
-    className="py-[50px]"
-  >
-    <TopNavBar 
-      title="National Statistics"
-      showAccount
-    />
-    <Suspense fallback={<span>...Loading</span>}>
-      <div className="p-2 flex flex-col">
-        <Link
-            className="cursor-pointer"
-            href={`${PUBLIC_LEADERBOARD}`}
-          >
-            <VscArrowLeft
-              className="text-epp-indigo w-10 h-10"
-            />
-        </Link>
-        <NationalStatisticsBarChart 
-          chartData={chartData}
-        />
-      </div>
-      <div
-        className="flex flex-col p-4"
-      >
-        <span className="font-bold text-3xl">{nationalStats.totalWHSOfTheNation}</span>
-        <span className="font-normal text-sm">WHS/day</span>
-      </div>
-    </Suspense>
-    <BottomTabBar />
-  </section>
+  return (
+    <section className="py-[50px]">
+      <TopNavBar title="National Statistics" showAccount />
+      <Suspense fallback={<span>...Loading</span>}>
+        <div className="p-2 flex flex-col">
+          <Link className="cursor-pointer" href={`${PUBLIC_LEADERBOARD}`}>
+            <VscArrowLeft className="text-epp-indigo w-10 h-10" />
+          </Link>
+          <NationalStatisticsBarChart chartData={chartData} />
+        </div>
+        <div className="flex flex-col p-4">
+          <span className="font-bold text-3xl">
+            {nationalStats.totalWHSOfTheNation}
+          </span>
+          <span className="font-normal text-sm">WHS/day</span>
+        </div>
+      </Suspense>
+      {/* <BottomTabBar /> */}
+    </section>
+  );
 }
--- components/location/LocationCard.tsx
+++ components/location/LocationCard.tsx
@@ -1,32 +1,25 @@
-'use client'
+"use client";
 
 import { LocationClass, LocationType } from "@/types/location.type";
-
+import PublicBadge from "./PublicBadge";
 /**
  * @param {location} location data to display to cards.
  * @param {isLink} isLink boolean show correct cursor pointer if linkable
  */
 type Props = {
   location: LocationType;
-  isLink?: boolean
-}
-const LocationCard = ({location, isLink}: Props) => {
-  const {
-    streetAddress,
-    city,
-    state,
-    totalAppliances,
-    totalWHSPerDay,
-  } = new LocationClass(location);
+  isLink?: boolean;
+};
+const LocationCard = ({ location, isLink }: Props) => {
+  const { streetAddress, city, state, totalAppliances, totalWHSPerDay, id } =
+    new LocationClass(location);
   const fullLocation = `${streetAddress} ${city} ${state}`;
   return (
-    <div 
+    <div
       className={`rounded-lg bg-papaya flex flex-col p-4 max-h-48 w-full space-y-8
-        ${(isLink) ? 'cursor-pointer' : 'cursor-default'}`}
+        ${isLink ? "cursor-pointer" : "cursor-default"}`}
     >
-      <span
-        className="font-bold text-2xl text-pretty line-clamp-2"
-      >
+      <span className="font-bold text-2xl text-pretty line-clamp-2">
         {fullLocation}
       </span>
 
@@ -37,11 +30,11 @@ const LocationCard = ({location, isLink}: Props) => {
         </span>
         <span className="font-normal space-x-1">
           <span className="font-bold">{totalWHSPerDay}</span>
-          <span>{'whs/day'}</span>
+          <span>{"whs/day"}</span>
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LocationCard;
